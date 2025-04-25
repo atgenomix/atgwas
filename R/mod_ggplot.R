204517ceb7c2c6ab0ab222b1d23b34d651991b7b
charleschuang1993 <charleschuang1993@gmail.com>
2025-04-25 17:20:11 +0800

    Add plot_manhattan_bg function for enhanced Manhattan plot visualization; include chromosome positioning and tooltips

--- R/mod_ggplot.R
+++ R/mod_ggplot.R
@@ -72,7 +72,68 @@ plot_manhattan <- function(df,
 }
 
 
+
+plot_manhattan_bg <- function(df, chr_col = "CHR", bp_col = "BP", p_col = "P") {
+    df2 <- df %>%
+      mutate(
+        CHR = as.numeric(.data[[chr_col]]),
+        BP  = as.numeric(.data[[bp_col]]),
+        P   = as.numeric(.data[[p_col]])
+      ) %>%
+      filter(!is.na(P))
+    chr_info <- df2 %>%
+      group_by(chr = CHR) %>%
+      summarise(chr_len = max(BP, na.rm = TRUE), .groups = "drop") %>%
+      arrange(chr) %>%
+      mutate(cum_start = cumsum(lag(chr_len, default = 0)))
+    df2 <- df2 %>%
+      left_join(chr_info %>% select(chr, cum_start),
+                by = c("CHR" = "chr")) %>%
+      arrange(CHR, BP) %>%
+      mutate(pos_cum = BP + cum_start)
+    axis_df <- df2 %>%
+      group_by(CHR) %>%
+      summarise(center = (min(pos_cum) + max(pos_cum)) / 2, .groups = "drop")
+    ggplot(df2, aes(x = pos_cum, y = -log10(P),
+                    color = as.factor(CHR),
+                    text  = tooltip)) +
+      geom_point(alpha = 0.6, size = 1) +
+      scale_x_continuous(breaks = axis_df$center,
+                         labels = axis_df$CHR) +
+      scale_y_continuous(expand = c(0, 0)) +
+      labs(
+        x     = "Chromosome",
+        y     = "-log10(P)",
+        title = "GWAS Manhattan Plot"
+      ) +
+      theme_bw() +
+      theme(
+        legend.position    = "none",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()
+      )
+  }
+library(dplyr)
+library(ggplot2)
+  
+
 assoc <- read.table("/Users/charleschuang/Downloads/output_gwas_results.assoc.logistic", header = TRUE, stringsAsFactors = FALSE)
 manhattan_plot <- plot_manhattan(assoc)
+
+
+assoc$tooltip <- paste0(
+  "CHR: ", assoc$CHR,
+  "\nBP: ", assoc$BP,
+  "\nP: ", signif(assoc$P, 3),
+  if ("SNP" %in% names(assoc)) paste0("\nSNP: ", assoc$SNP) else ""
+)
+manhattan_plot <- plot_manhattan_bg(assoc)
+
 print(manhattan_plot)
 ggsave("manhattan_plot.png", manhattan_plot, width = 10, height = 5, dpi = 300)
+
+
+
+
+
+
